7ed0dae63ba5a4b5e30ff90082ddaba589651c41
matiasnav710 <navarretematias710@gmail.com>
2020-02-14 22:20:39 +0400

    add jwt service

---
+++ src/app/api.js
@@ -0,0 +1,173 @@
+import Axios from 'axios';
+import jwtDecode from 'jwt-decode';
+
+const axios = Axios.create({
+    baseURL: window.location.host === 'localhost:3000' ? 'http://localhost:8080' : '/',
+    timeout: 5000,
+    headers: {'X-Custom-Header': 'foobar'}
+  });
+
+class jwtService {
+
+    init()
+    {
+        this.setInterceptors();
+        this.handleAuthentication();
+    }
+
+    emit() {
+      console.info('jwtService.emit - ', arguments)
+    }
+
+    setInterceptors = () => {
+        axios.interceptors.response.use(response => {
+            return response;
+        }, err => {
+            return new Promise((resolve, reject) => {
+                if ( err && err.response && err.response.status === 401 && err.config && !err.config.__isRetryRequest )
+                {
+                    // if you ever get an unauthorized response, logout the user
+                    this.emit('onAutoLogout', 'Invalid access_token');
+                    this.setSession(null);
+                }
+                throw err;
+            });
+        });
+    };
+
+    handleAuthentication = () => {
+
+        let access_token = this.getAccessToken();
+
+        if ( !access_token )
+        {
+            this.emit('onNoAccessToken');
+
+            return;
+        }
+
+        if ( this.isAuthTokenValid(access_token) )
+        {
+            this.setSession(access_token);
+            this.emit('onAutoLogin', true);
+        }
+        else
+        {
+            this.setSession(null);
+            this.emit('onAutoLogout', 'access_token expired');
+        }
+    };
+
+    createUser = (data) => {
+        return new Promise((resolve, reject) => {
+            axios.post('/api/auth/register', data)
+                .then(response => {
+                    if ( response.data.user )
+                    {
+                        this.setSession(response.data.access_token);
+                        resolve(response.data.user);
+                    }
+                    else
+                    {
+                        reject(response.data.error);
+                    }
+                });
+        });
+    };
+
+    signInWithEmailAndPassword = (email, password) => {
+        return new Promise((resolve, reject) => {
+            axios.post('/api/auth/login', {
+                email,
+                password
+            }).then(response => {
+                if ( response.data.user )
+                {
+                    this.setSession(response.data.access_token);
+                    resolve(response.data.user);
+                }
+                else
+                {
+                    reject(response.data.error);
+                }
+            }).catch(e => {
+                reject(e);
+            });
+        });
+    };
+
+    signInWithToken = () => {
+        return new Promise((resolve, reject) => {
+            axios.get('/api/auth/user', {
+                headers: {
+                    Authorization: `Bearer ${this.getAccessToken()}`
+                }
+            })
+                .then(response => {
+                    if ( response.data.user )
+                    {
+                        this.setSession(response.data.access_token);
+                        resolve(response.data.user);
+                    }
+                    else
+                    {
+                        this.logout();
+                        reject('Failed to login with token.');
+                    }
+                })
+                .catch(error => {
+                    this.logout();
+                    reject('Failed to login with token.');
+                });
+        });
+    };
+
+    updateUserData = (user) => {
+        return axios.post('/api/auth/user/update', {
+            user: user
+        });
+    };
+
+    setSession = access_token => {
+        if ( access_token )
+        {
+            localStorage.setItem('jwt_access_token', access_token);
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
+        }
+        else
+        {
+            localStorage.removeItem('jwt_access_token');
+            delete axios.defaults.headers.common['Authorization'];
+        }
+    };
+
+    logout = () => {
+        this.setSession(null);
+    };
+
+    isAuthTokenValid = access_token => {
+        if ( !access_token )
+        {
+            return false;
+        }
+        const decoded = jwtDecode(access_token);
+        const currentTime = Date.now() / 1000;
+        if ( decoded.exp < currentTime )
+        {
+            console.warn('access token expired');
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    };
+
+    getAccessToken = () => {
+        return window.localStorage.getItem('jwt_access_token');
+    };
+}
+
+const instance = new jwtService();
+
+export default instance;
