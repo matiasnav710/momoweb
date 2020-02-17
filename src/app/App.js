51bd5ad1678bfaa52f272405057c71dbeb717c52
matiasnav710 <navarretematias710@gmail.com>
2020-02-17 19:06:50 +0400

    login with email

--- src/app/App.js
+++ src/app/App.js
@@ -1,62 +1,102 @@
-import React, { Component, Fragment } from 'react';
-import { withRouter } from 'react-router-dom';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { withRouter } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './App.scss';
-import AppRoutes from './AppRoutes';
-import Navbar from './shared/Navbar';
-import Sidebar from './shared/Sidebar';
-import Footer from './shared/Footer';
+import "./App.scss";
+import AppRoutes from "./AppRoutes";
+import Navbar from "./shared/Navbar";
+import Sidebar from "./shared/Sidebar";
+import Footer from "./shared/Footer";
 import { withTranslation } from "react-i18next";
-import Login from './user-pages/Login';
-import Spinner from '../app/shared/Spinner';
+import Login from "./user-pages/Login";
+import Spinner from "../app/shared/Spinner";
+import { AuthActions } from "./store";
 
 class App extends Component {
   state = {
     authenticated: false,
     loading: true
-  }
+  };
 
   componentDidMount() {
-    setTimeout(() => {
+    if (this.props.loading) {
+      this.setState({
+        loading: true,
+        authenticated: false
+      });
+    } else if (this.props.authenticated) {
+      this.setState({
+        loading: false,
+        authenticated: true
+      });
+    } else {
       this.setState({
         loading: false,
         authenticated: false
-      })
-    }, 3000)
+      });
+    }
   }
 
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (this.props.loading) {
+      this.setState({ loading: true, authenticated: false });
+    } else if (nextProps.authenticated) {
+      this.setState({ loading: false, authenticated: true });
+    }
+  }
+
+  onLogout = () => {
+    this.props.setAuthenticated(false);
+    this.props.setLoading(false);
+    this.setState({ loading: false, authenticated: false });
+  };
+
   render() {
-    return <Switch>
-      <Route exact path="/login" component={ Login } />
-      <ProtectedApp{...this.props} loading={this.state.loading} authenticated={this.state.authenticated}/>
-    </Switch>
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedApp
+          {...this.props}
+          loading={this.state.loading}
+          authenticated={this.state.authenticated}
+          onLogout={this.onLogout}
+        />
+      </Switch>
+    );
   }
 }
 
 class ProtectedApp extends Component {
-  state = {}
+  state = {};
 
-  render () {
+  onLogout = () => {
+    this.props.onLogout();
+  };
 
+  render() {
     if (this.props.loading) {
-      return <Spinner/>
+      return <Spinner />;
     } else if (!this.props.authenticated) {
-      return <Redirect to="/login"/>
+      return <Redirect to="/login" />;
     }
-    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
-    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
-    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
+    let navbarComponent = !this.state.isFullPageLayout ? (
+      <Navbar onLogout={this.onLogout} />
+    ) : (
+      ""
+    );
+    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar /> : "";
+    let footerComponent = !this.state.isFullPageLayout ? <Footer /> : "";
     return (
       <div className="container-scroller">
-        { sidebarComponent }
+        {sidebarComponent}
         <div className="container-fluid page-body-wrapper">
-          { navbarComponent }
+          {navbarComponent}
           <div className="main-panel">
             <div className="content-wrapper">
-              <AppRoutes/>
+              <AppRoutes />
             </div>
-            { footerComponent }
+            {footerComponent}
           </div>
         </div>
       </div>
@@ -64,4 +104,16 @@ class ProtectedApp extends Component {
   }
 }
 
-export default withTranslation()(withRouter(App));
+const mapDispatchToProps = {
+  setAuthenticated: AuthActions.setAuthenticated,
+  setLoading: AuthActions.setLoading
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated,
+  loading: state.auth.loading
+});
+
+export default withTranslation()(
+  withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
 import "./i18n";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./app/store/createStore";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
-, document.getElementById('root'));
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
