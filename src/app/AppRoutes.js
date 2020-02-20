2bfc0cbf61091b13e8ededd51eafe1215358a98c
matiasnav710 <navarretematias710@gmail.com>
2020-02-20 16:20:24 +0400

    clean up app routes

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,6 +1,5 @@
-import React, { Component, Fragment, lazy } from "react";
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Spinner from "../app/shared/Spinner";
 
 import Dashboard from "./dashboard/Dashboard";
 import Settings from "./settings/Settings";
@@ -8,11 +7,11 @@ import Settings from "./settings/Settings";
 class AppRoutes extends Component {
   render() {
     return (
-      <Fragment>
+      <Switch>
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/settings" component={Settings} />
         <Redirect to="/dashboard" />
-      </Fragment>
+      </Switch>
     );
   }
 }
