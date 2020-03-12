1ca30d0962d80bec7969778bdf57e11d68ae3138
matiasnav710 <navarretematias710@gmail.com>
2020-03-12 20:27:01 +0400

    app/user-pages/Payment: create

---
+++ src/app/user-pages/Payment.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Form } from "react-bootstrap";
+import { connect } from "react-redux";
+import { AuthActions } from "../store";
+import Api from "../api";
+import i18n from "../../i18n";
+
+class Payment extends Component {
+  state = {
+    errTxt: '',
+    succTxt: ''
+  };
+
+  componentWillUpdate() {
+    console.info('verification page update');
+    if (this.props.user && this.props.user.email_verified) {
+      this.props.history.push('/dashboard')
+    }
+  }
+
+  onResend = () => {
+    this.setState({ errTxt: "" });
+    Api.verify(this.props.user.email)
+      .then(() => {
+        this.setState({ errTxt: '', succTxt: 'Sent successfully' })
+      })
+      .catch(error => {
+        let errTxt = error.toString()
+        if (error.toString() === 'TypeError: Failed to fetch') {
+          errTxt = 'Service not available';
+        } else {
+          errTxt = i18n.getResource("en", ["translations"], errTxt);
+          if (!lerrTxt) {
+            errTxt = "Unknown problem";
+          }
+        }
+        this.setState({ errTxt, succTxt: '' });
+      });
+  };
+
+  render() {
+    const { errTxt, succTxt } = this.state;
+    return (
+      <div>
+        <div className="d-flex align-items-center auth px-0">
+          <div className="row w-100 mx-0">
+            <div className="col-lg-4 mx-auto">
+              <div className="card text-left py-5 px-4 px-sm-5">
+                <div className="brand-logo">
+                  <h2>MomoWeb</h2>
+                </div>
+                <h4>Subscription</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  setAuthenticated: AuthActions.setAuthenticated,
+  setLoading: AuthActions.setLoading,
+  setUser: AuthActions.setUser
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated,
+  user: state.auth.user
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Payment));
