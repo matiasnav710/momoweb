b22c3bd63b47ad966f7e4a9c5ef66d6503947264
matiasnav710 <navarretematias710@gmail.com>
2020-03-13 01:46:41 +0400

    app/user-pages/Subscription: subscription form

--- src/app/user-pages/Subscription.js
+++ src/app/user-pages/Subscription.js
@@ -14,23 +14,49 @@ import i18n from "../../i18n";
 class Subscription extends Component {
   state = {
     errTxt: '',
-    succTxt: ''
+    succTxt: '',
+    plans: []
   };
+
+  componentDidMount() {
+    // get plans
+    this.getPlans()
+  }
 
   componentWillUpdate() {
 
+  }
+
+  getPlans = async () => {
+    const plans = await Api.getStripePlans()
+    this.setState({ plans })
   }
 
   render() {
     const { errTxt, succTxt } = this.state;
     return (
       <div>
-        <div className="d-flex align-items-center auth px-0">
+        <div className="align-items-center auth px-0">
+          <div className="row">
+            <h2 className="col-12 text-center">Subscription</h2>
+          </div>
+          <div className="row">
+            {this.state.plans.map((plan) => {
+              return <div className="col-md-3 text-center p-2" key={plan.id}>
+                <div className="card p-2">
+                  <h4>{plan.name}</h4>
+                  <p>${plan.amount / 100} / {plan.interval}</p>
+                </div>
+              </div>
+            })
+            }
+          </div>
           <div className="row w-100 mx-0">
             <div className="col-lg-6 mx-auto">
-              <div className="card text-center md-6 p-4">
-                <div className="brand-logo">
-                  <h2>Subscription</h2>
+              <div className="card text-center p-4">
+
+                <div className="card p-2">
+                  <div className="row"></div>
                 </div>
                 <div id="paymentForm" className="m-4">
                   <Elements stripe={stripePromise}>
