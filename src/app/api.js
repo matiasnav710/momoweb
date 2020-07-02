878961ad312028dc57a04a155d770f82c3db88a7
matiasnav710 <navarretematias710@gmail.com>
2020-07-03 02:35:31 +0800

    fix api path for discovery table

--- src/app/api.js
+++ src/app/api.js
@@ -14,7 +14,7 @@ const axios = Axios.create({
   headers: { "Content-Type": "application/json", Accept: "application/json" }
 });
 
-const STATS_API = 'https://beta-data.mometic.com/api/stats'
+const STATS_API = 'https://beta-data.mometic.com/api/discovery'
 
 class API {
   init() {
