235b867343e1de36814323c25d09828cef017734
matiasnav710 <navarretematias710@gmail.com>
2020-07-07 04:26:32 +0800

    export SECTORS_FILTER

--- src/app/constants.js
+++ src/app/constants.js
@@ -3,3 +3,58 @@ export const PRICE_MAX = 3001
 
 export const AVG_VOL_MIN = 0
 export const AVG_VOL_MAX = 20001 // Max 20000K
+
+
+export const SECTORS_FILTER = {
+    'BASIC INDUSTRIES': {
+        'Process Industries': true,
+        'Conglomerates': true,
+        'Industrials': true,
+        'Basic Industries': true,
+        'Basic Materials': true,
+    },
+    'CAPITAL GOODS': {
+        'Capital Goods': true,
+    },
+    'CONSUMER GOODS': {
+        'Consumer Non Durables': true,
+        'Consumer Non-Durables': true,
+        'Consumer Defensive': true,
+        'Consumer Cyclical': true,
+        'Consumer Durables': true,
+    },
+    'SERVICES': {
+        'Consumer': true,
+        'Services': true,
+        'Commercial Services': true,
+        'Communications Services': true,
+    },
+    'ENERGY': {
+        Energy: true,
+    },
+    'FINANCE': {
+        'Finance': true,
+        'Financial Services': true,
+    },
+    'HEALTH CARE': {
+        'Health Care': true,
+        'Health Technology': true,
+        'Heathcare': true,
+    },
+    'PUBLIC UTILITIES': {
+        'Public Utilities': true,
+        'Utilities': true,
+    },
+    'COMMUNICATIONS': {
+        Coummunications: true,
+        Technology: true,
+    },
+    'TRANSPORTION': {
+        Transportation: true,
+    },
+    'MISCELLANEOUS': {
+        'Miscellaneous': true,
+        'n/a': true,
+        'Real Estate': true,
+    },
+};
