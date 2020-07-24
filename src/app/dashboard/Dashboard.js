2abd1e9f0af369d68fd79109de59f2e61e99e8fb
matiasnav710 <navarretematias710@gmail.com>
2020-07-24 12:39:36 -0400

    feat: change the date format to mm/dd/yy

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -1004,14 +1004,14 @@ export class Dashboard extends Component {
 
     let d = date;
     d = [
-      '0' + d.getDate(),
       '0' + (d.getMonth() + 1),
+      '0' + d.getDate(),
       '' + d.getFullYear(),
       '0' + d.getHours(),
       '0' + d.getMinutes(),
     ].map((component) => component.slice(-2));
 
-    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+    return d.slice(0, 3).join('/') + ' ' + d.slice(3).join(':');
   }
 
   renderAlertHistory = () => {
