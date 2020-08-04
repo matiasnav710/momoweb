e754a278c60489a06f0a94d1dd2ee90ecf84d207
matiasnav710 <navarretematias710@gmail.com>
2020-08-04 18:41:09 +0400

    export funcs

--- src/app/settings/LogSlider.js
+++ src/app/settings/LogSlider.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Slider from 'nouislider-react';
 
-function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
+export function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
 
     var minv = Math.log(_minv);
     var maxv = Math.log(_maxv);
@@ -14,7 +14,7 @@ function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000
     return Math.exp(minv + scale * (position - minp));
 }
 
-function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
+export function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
 
     // The result should be between 100 an 10000000
     var minv = Math.log(_minv);
@@ -26,8 +26,14 @@ function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000)
     return (Math.pow(Math.E, value) - minv) / scale + minp
 }
 
-export default function ({ ...props }) {
-    return <Slider 
-        range={{min: minp,}}
-    {...props} />
+export default function ({ start, onChange, ...props }) {
+    return <Slider
+        {...props}
+        range={{ min: 0, max: 100 }}
+        start={[fromValue(start[0]), fromValue(start[1])]}
+        onChange={(render, handle, value, un, percent) => {
+            console.info('On Change:', value)
+            onChange(render, handle, value.map(toValue), un, percent)
+        }}
+    />
 }
