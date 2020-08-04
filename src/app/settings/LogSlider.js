1303e7a74059861a533baf6fff151abc4cb06e13
matiasnav710 <navarretematias710@gmail.com>
2020-08-04 11:21:03 +0400

    remove min/max p

--- src/app/settings/LogSlider.js
+++ src/app/settings/LogSlider.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Slider from 'nouislider-react';
 
-
 function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
-    // position will be between 0 and 100
-    var minp = 0;
-    var maxp = 100;
 
-    // The result should be between 100 an 10000000
     var minv = Math.log(_minv);
-    var maxv = Math.log(10000000);
+    var maxv = Math.log(_maxv);
+    // The result should be between 100 an 10000000
+
 
     // calculate adjustment factor
     var scale = (maxv - minv) / (maxp - minp);
@@ -18,9 +15,6 @@ function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000
 }
 
 function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
-    // position will be between 0 and 100
-    var minp = 0;
-    var maxp = 100;
 
     // The result should be between 100 an 10000000
     var minv = Math.log(_minv);
@@ -32,7 +26,8 @@ function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000)
     return (Math.pow(Math.E, value) - minv) / scale + minp
 }
 
-
 export default function ({ ...props }) {
-    return <Slider {...props} />
+    return <Slider 
+        range={{min: minp,}}
+    {...props} />
 }
