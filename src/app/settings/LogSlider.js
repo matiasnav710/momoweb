f6b28df96d8eeb8e2e0b782b25487ffb4cfd993d
matiasnav710 <navarretematias710@gmail.com>
2020-08-04 10:44:49 +0400

    add toValue

--- src/app/settings/LogSlider.js
+++ src/app/settings/LogSlider.js
@@ -17,6 +17,22 @@ function toValue(position, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000
     return Math.exp(minv + scale * (position - minp));
 }
 
+function fromValue(value, minp = 0, maxp = 100, _minv = 100000, _maxv = 2000000) {
+    // position will be between 0 and 100
+    var minp = 0;
+    var maxp = 100;
+
+    // The result should be between 100 an 10000000
+    var minv = Math.log(_minv);
+    var maxv = Math.log(10000000);
+
+    // calculate adjustment factor
+    var scale = (maxv - minv) / (maxp - minp);
+
+    return (Math.pow(Math.E, value) - minv) / scale + minp
+}
+
+
 export default function ({ ...props }) {
     return <Slider {...props} />
 }
