12bbc66e20b70467d47a7cdf8a70f2bf93edf809
matiasnav710 <navarretematias710@gmail.com>
2020-08-10 20:14:15 +0400

    remove unused dropdown

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -73,68 +73,6 @@ class Navbar extends Component {
             </div>
           </div>
           <ul className='navbar-nav navbar-nav-right'>
-            <Dropdown alignRight as='li' className='nav-item d-none d-lg-block'>
-              <Dropdown.Menu className='navbar-dropdown preview-list create-new-dropdown-menu'>
-                <h6 className='p-3 mb-0'>
-                  <Trans>Projects</Trans>
-                </h6>
-                <Dropdown.Divider />
-                <Dropdown.Item
-                  href='!#'
-                  onClick={(evt) => evt.preventDefault()}
-                  className='preview-item'
-                >
-                  <div className='preview-thumbnail'>
-                    <div className='preview-icon bg-dark rounded-circle'>
-                      <i className='mdi mdi-file-outline text-primary'></i>
-                    </div>
-                  </div>
-                  <div className='preview-item-content'>
-                    <p className='preview-subject ellipsis mb-1'>
-                      <Trans>Software Development</Trans>
-                    </p>
-                  </div>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item
-                  href='!#'
-                  onClick={(evt) => evt.preventDefault()}
-                  className='preview-item'
-                >
-                  <div className='preview-thumbnail'>
-                    <div className='preview-icon bg-dark rounded-circle'>
-                      <i className='mdi mdi-web text-info'></i>
-                    </div>
-                  </div>
-                  <div className='preview-item-content'>
-                    <p className='preview-subject ellipsis mb-1'>
-                      <Trans>UI Development</Trans>
-                    </p>
-                  </div>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item
-                  href='!#'
-                  onClick={(evt) => evt.preventDefault()}
-                  className='preview-item'
-                >
-                  <div className='preview-thumbnail'>
-                    <div className='preview-icon bg-dark rounded-circle'>
-                      <i className='mdi mdi-layers text-danger'></i>
-                    </div>
-                  </div>
-                  <div className='preview-item-content'>
-                    <p className='preview-subject ellipsis mb-1'>
-                      <Trans>Software Testing</Trans>
-                    </p>
-                  </div>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <p className='p-3 mb-0 text-center'>
-                  <Trans>See all projects</Trans>
-                </p>
-              </Dropdown.Menu>
-            </Dropdown>
             <Dropdown
               alignRight
               as='li'
