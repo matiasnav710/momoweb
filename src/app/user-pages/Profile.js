84a867043771118aa9f1de10cc406f52b056a2dd
matiasnav710 <navarretematias710@gmail.com>
2020-08-21 23:18:24 +0400

    style profile page

--- src/app/user-pages/Profile.js
+++ src/app/user-pages/Profile.js
@@ -9,106 +9,131 @@ import i18n from '../../i18n';
 import cogoToast from 'cogo-toast';
 
 export class Profile extends Component {
-    state = {
-        errTxt: '',
-    };
+  state = {
+    errTxt: '',
+  };
 
-    componentDidMount() {
-        window.localStorage.removeItem('jwt_access_token')
+  componentDidMount() {
+    window.localStorage.removeItem('jwt_access_token')
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault()
+    const query = qs.parse(this.props.location.search, { ignoreQueryPrefix: true })
+    console.info('Props:', query)
+    const { token } = query
+    const password = this.refPassword.value
+    const rePassword = this.refConfirm.value
+    if (password.length < 6) {
+      return cogoToast.error('At least 6 characters required!')
     }
+    if (password != rePassword) {
+      return cogoToast.error('Password not matched!')
+    }
+    await Api.resetPassword(password, token)
+    cogoToast.success('The password is successfully reset! You will be redirected to the login page.')
+    setTimeout(() => {
+      this.props.history.replace('/login')
+    }, 3000)
+  };
 
-    onSubmit = async (e) => {
-        e.preventDefault()
-        const query = qs.parse(this.props.location.search, { ignoreQueryPrefix: true })
-        console.info('Props:', query)
-        const { token } = query
-        const password = this.refPassword.value
-        const rePassword = this.refConfirm.value
-        if (password.length < 6) {
-            return cogoToast.error('At least 6 characters required!')
-        }
-        if (password != rePassword) {
-            return cogoToast.error('Password not matched!')
-        }
-        await Api.resetPassword(password, token)
-        cogoToast.success('The password is successfully reset! You will be redirected to the login page.')
-        setTimeout(() => {
-            this.props.history.replace('/login')
-        }, 3000)
-    };
-
-    render() {
-        const { errTxt } = this.state;
-        return (
-            <div>
-                <div className='d-flex align-items-center auth px-0 h-100'>
-                    <div className='row w-100 mx-0'>
-                        <div className='col-lg-4 mx-auto'>
-                            <div className='card text-left py-5 px-4 px-sm-5'>
-                                <div className='brand-logo'>
-                                    <span className='h2 pr-2'>MOMO</span>
-                                </div>
-                                <h4>Profile</h4>
-                                <form className='pt-3' onSubmit={this.onSubmit}>
-                                    <Form.Group>
-                                        <label>Password</label>
-                                        <div className='input-group'>
-                                            <div className='input-group-prepend'>
-                                                <i className='input-group-text mdi mdi-lock text-success' />
-                                            </div>
-                                            <Form.Control type='password' className='form-control text-light' placeholder='Password'
-                                                ref={ref => {
-                                                    this.refPassword = ref;
-                                                }}
-                                            />
-                                        </div>
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <label>Confirm Password</label>
-                                        <div className='input-group'>
-                                            <div className='input-group-prepend'>
-                                                <i className='input-group-text mdi mdi-lock text-success' />
-                                            </div>
-                                            <Form.Control type='password' className='form-control text-light' placeholder='Password'
-                                                ref={ref => {
-                                                    this.refConfirm = ref;
-                                                }}
-                                            />
-                                        </div>
-                                    </Form.Group>
-                                    <div className='mt-3'>
-                                        <button
-                                            type='submit'
-                                            className='btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn'
-                                        >
-                                            RESET
-                                        </button>
-                                    </div>
-                                    {errTxt !== '' && (
-                                        <label className='text-danger'>{`${errTxt}`}</label>
-                                    )}
-
-                                    <div className='text-center mt-4 font-weight-light'>
-                                        <Link to='/login' className='text-primary'>Login</Link>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    const { errTxt } = this.state;
+    return (
+      <div>
+        <div className='d-flex align-items-center auth px-0 h-100'>
+          <div className='row w-100 mx-0'>
+            <div className='col-lg-4 mx-auto'>
+              <div className='card text-left py-5 px-4 px-sm-5'>
+                <div className='brand-logo'>
+                  <span className='h2 pr-2'>PROFILE</span>
                 </div>
+                <form className='pt-3' onSubmit={this.onSubmit}>
+
+                  <Form.Group>
+                    <label>Name</label>
+                    <div className='input-group'>
+                      <div className='input-group-prepend'>
+                        <i className='input-group-text mdi mdi-account text-success' />
+                      </div>
+                      <Form.Control className='form-control text-light' placeholder='John Doe'
+                        ref={ref => {
+                          this.refConfirm = ref;
+                        }}
+                      />
+                    </div>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <label>Email</label>
+                    <div className='input-group'>
+                      <div className='input-group-prepend'>
+                        <i className='input-group-text mdi mdi-email text-success' />
+                      </div>
+                      <Form.Control className='form-control text-light' placeholder='Email'
+                        ref={ref => {
+                          this.refConfirm = ref;
+                        }}
+                      />
+                    </div>
+                  </Form.Group>
+
+
+                  <Form.Group>
+                    <label>Password</label>
+                    <div className='input-group'>
+                      <div className='input-group-prepend'>
+                        <i className='input-group-text mdi mdi-lock text-success' />
+                      </div>
+                      <Form.Control type='password' className='form-control text-light' placeholder='Password'
+                        ref={ref => {
+                          this.refPassword = ref;
+                        }}
+                      />
+                    </div>
+                  </Form.Group>
+                  <Form.Group>
+                    <label>Confirm Password</label>
+                    <div className='input-group'>
+                      <div className='input-group-prepend'>
+                        <i className='input-group-text mdi mdi-lock text-success' />
+                      </div>
+                      <Form.Control type='password' className='form-control text-light' placeholder='Confirm Password'
+                        ref={ref => {
+                          this.refConfirm = ref;
+                        }}
+                      />
+                    </div>
+                  </Form.Group>
+                  <div className='mt-3'>
+                    <button
+                      type='submit'
+                      className='btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn'
+                    >
+                      SAVE
+                                        </button>
+                  </div>
+                  {errTxt !== '' && (
+                    <label className='text-danger'>{`${errTxt}`}</label>
+                  )}
+                </form>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapDispatchToProps = {
-    setAuthenticated: AuthActions.setAuthenticated,
-    setLoading: AuthActions.setLoading,
-    setUser: AuthActions.setUser,
+  setAuthenticated: AuthActions.setAuthenticated,
+  setLoading: AuthActions.setLoading,
+  setUser: AuthActions.setUser,
 };
 
 const mapStateToProps = state => ({
-    authenticated: state.auth.authenticated
+  authenticated: state.auth.authenticated
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Profile));
